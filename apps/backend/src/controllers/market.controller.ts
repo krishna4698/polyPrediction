a89f632bdcf3f6e63d34df86b8244d1eb7a35fad
krishna joshi <krishnajoshi4698@gmail.com>
2026-06-15 02:11:25 +0530

    create market complete

--- apps/backend/src/controllers/market.controller.ts
+++ apps/backend/src/controllers/market.controller.ts
@@ -1,23 +1,38 @@
 import { prisma } from "db";
+import  type { Request, Response } from "express";
 import { createMarketSchema } from "../schemas/market.schema.js"
 
 export const createMarket =async (req:Request,res:Response)=>{
-    try{
-     const data = createMarketSchema.safeParse(req.body);
+  
+     const result  = createMarketSchema.safeParse(req.body);
+
+     if(!result.success){
+        return res.status(400).json({
+            message :"invalied market data"
+        })
+     }
+
+       try{
 
      const  market = await prisma.market.create({
-        data:{
-            title:data.title,
-            description:data.description,
-            start_time:data.opensAt,
-            end_time:data.closesAt,
-            outc
+            data:{
+               title:result.data.title,
+               description:result.data.description,
+               start_time:result.data.opensAt,
+               end_time:result.data.closesAt,
+               outcomes:{
+                create:result.data.outcomes,
+               },
 
-        }
+            },
+            include:{
+                outcomes:true
+            }
      })
+     return res.json(market)
     }
     catch(e){
-
+      res.json(e);
     }
 
 }
