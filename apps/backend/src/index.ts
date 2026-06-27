027a6238ad8eabdf9dd80d082aaf18383b78af5b
krishna joshi <krishnajoshi4698@gmail.com>
2026-06-28 02:40:45 +0530

    updated

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,9 +2,15 @@ import express from "express"
 import {prisma} from "db"
 import orderRoute from "./routes/order.route.js"
 import marketRoute from "./routes/market.route.js"
+import authRoute from "./routes/auth.routes.js"
 const app = express();
 
-
+app.use(express.json());
+app.use("/", authRoute)
 app.use("/order", orderRoute )
 app.use("/market", marketRoute )
+
+
+app.listen(3000)
+console.log("server is running on port 3000");
 
