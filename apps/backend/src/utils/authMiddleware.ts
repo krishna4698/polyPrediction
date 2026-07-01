598e9dbbedfd47f41891e56a64d97cb53d32b6e8
krishna joshi <krishnajoshi4698@gmail.com>
2026-07-02 00:42:08 +0530

    added auth middleware

---
+++ apps/backend/src/utils/authMiddleware.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+
+declare module "express-serve-static-core"{
+    interface Request{
+        user:string | jwt.JwtPayload
+    }
+}
+
+export const authMiddleware= (req: Request, res: Response, next:NextFunction) => {
+      try{
+        // const token =req.cookies.token("token");
+        const token =req.headers.authorization
+    console.log("this is token", token);
+
+      if(!token){
+        return res.status(404).send("Please login first");
+      }
+      const verified= jwt.verify(token, "secret")
+      req.user=verified
+      next();
+      }
+      catch(err){
+        return res.status(401).send("expired Token");
+      }      
+}
