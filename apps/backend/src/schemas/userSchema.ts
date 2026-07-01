dc63a1fbf2d8fcdf7356cdf924c050c9f7145dbb
krishna joshi <krishnajoshi4698@gmail.com>
2026-07-02 00:42:49 +0530

    update userSchema

--- apps/backend/src/schemas/userSchema.ts
+++ apps/backend/src/schemas/userSchema.ts
@@ -1,8 +1,16 @@
-import {z, ZodLazy} from "zod"
+import { z } from "zod"
 
-const userSchema = z.object({
+export const userSchema = z.object({
       email: z.string(),
       username: z.string(),
       password: z.string(),
       
 })
+
+export const loginSchema = z.object({
+      email: z.string(),
+      password: z.string(),
+})
+
+export type createUserType= z.infer<typeof userSchema>
+export type loginUserType = z.infer<typeof loginSchema>
